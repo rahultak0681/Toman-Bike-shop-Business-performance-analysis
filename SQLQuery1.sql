9c60c7e15ae2f7d6040924fc2e49c813f2247592
rahultak0681 <rahultak060801@gmail.com>
2024-07-24 20:57:18 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+
+with cte as (
+select * from bike_share_yr_0
+union all
+select * from bike_share_yr_1)
+
+select 
+dteday,
+season,
+a.yr,
+weekday,
+hr,
+rider_type,
+riders,
+price,
+COGS,
+riders*price as revenue,
+riders*price -COGS*riders as profit
+from cte a
+left join cost_table b
+on a.yr = b.yr
